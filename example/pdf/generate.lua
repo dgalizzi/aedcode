605cbf92053cc1bcd97733a8408040db41dad67e
Diego D. Galizzi <DGalizzi@gmail.com>
2011-08-12 14:15:20 -0300

    Separados por keyword los ejercicios
    El PDF de los ejercicios ahora se genera separando por las keywords que
    hay puestas en los .cpp de los ejercicios.

--- example/pdf/generate.lua
+++ example/pdf/generate.lua
@@ -7,37 +7,65 @@ preamble = [[\documentclass[10pt,a4paper]{article}
 
 print(preamble)
 
-n = 1
+body = {}
+
 handle = io.popen("ls ..")
 for f in handle:lines() do
 	if f:match(".*\.cpp") ~= nil then
 		if f ~= "util.cpp" and f ~= "util_tree.cpp" then
-			--print(f)
-			ejemplon = "\\paragraph{Ejercicio " ..  n .. "}"
-			n = n + 1
-			print(ejemplon)
 
 			file = io.open("../" .. f)
 			pr = false;
+			descr = ""
+			keys = {}
 			for lines in file:lines() do
 				if pr then
 					if lines:match(".*FIN.*") then
 						break
 					end
+
+
+					keywords = lines:match(".*keywords:(.*)")
+					if keywords then
+						for w in keywords:gmatch("%w+,") do
+							keys[#keys+1] = w
+						end
+						keys[#keys+1] = lines:match(".*,(.*)") or lines:match(".*:(.*)")
+						break
+					end
+
 					line_underscore_hacked = lines:gsub("_", "\\_")
-					print(line_underscore_hacked)
+					descr = descr .. line_underscore_hacked
 				elseif lines:match(".*COMIENZO.*") ~= nil then
 					pr = true
 				end
 			end
 			filename_underscore_hacked = f:gsub("_", "\\_")
-			print("Resuelto en el archivo {\\tt " .. filename_underscore_hacked .. "}")
 			print()
+			for i=1, #keys do
+				if body[keys[i]] == nil then
+					body[keys[i]] = {}
+				end
+				body[keys[i]][#body[keys[i]]+1]  = descr
+				body[keys[i]][#body[keys[i]]+1]  = filename_underscore_hacked
+			end
 			file:close()
 		end
 	end
 end
 
 handle:close()
+
+for k,v in pairs(body) do
+	print("\\section*{" .. k .. "}")
+	n = 1
+	while n <= #v do
+		ejemplon = "\\paragraph{Ejercicio " ..  n .. "}"
+		print(ejemplon)
+		print(v[n])
+		print("Resuelto en el archivo {\\tt " .. v[n+1] .. "}")
+		n = n + 2
+	end
+end
 
 print("\\end{document}")
